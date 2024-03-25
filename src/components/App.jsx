9dcba9ec9db242466dff178b6203408e44397134
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2024-03-25 17:41:08 +0100

    error vercel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 
 import './App.css';
-import FriendList from '../friendList/FriendList';
-import Profile from '../profile/Profile';
-import TransactionHistory from '../transactionHistory/transactionHistory';
-import userData from '../../userData.json';
-import friends from '../../friends.json';
-import transactions from '../../transactions.json';
+import FriendList from './friendList/FriendList';
+import Profile from './profile/Profile';
+import TransactionHistory from './transaction/TransactionHistory';
+import userData from '../userData.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
 
 function App() {
   return (
